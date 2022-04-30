07ae777c787a26001f7b8a2f2669bf0a64298786
criverao <c.riverao@uniandres.edu.co>
2022-04-30 07:31:14 -0500

    Crear el componente para listar cafés

--- src/app/cafe/cafe.module.ts
+++ src/app/cafe/cafe.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CafeListComponent } from './cafe-list/cafe-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CafeListComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    CafeListComponent
   ]
 })
 export class CafeModule { }
