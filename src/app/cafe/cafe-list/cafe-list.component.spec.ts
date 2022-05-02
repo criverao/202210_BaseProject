fb2987da8161392146b90b307bed0f2d538edd4f
criverao <c.riverao@uniandres.edu.co>
2022-05-01 22:43:30 -0500

    Probar componente verificando que se crea la tabla con un listado de tres cafés

--- src/app/cafe/cafe-list/cafe-list.component.spec.ts
+++ src/app/cafe/cafe-list/cafe-list.component.spec.ts
@@ -4,6 +4,8 @@ import { CafeListComponent } from './cafe-list.component';
 import { DebugElement } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { CafeService } from "../cafe.service";
+import { Cafe } from "../cafe";
+import faker from "@faker-js/faker";
 
 describe('CafeListComponent', () => {
   let component: CafeListComponent;
@@ -28,11 +30,68 @@ describe('CafeListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CafeListComponent);
     component = fixture.componentInstance;
+
+    for (let id=1; id <= 3; id++) {
+      component.cafes = [
+        new Cafe(
+          faker.datatype.number(),
+          faker.lorem.words(2),
+          faker.lorem.slug(),
+          faker.address.city(),
+          faker.lorem.sentence(),
+          faker.datatype.number(),
+          faker.image.imageUrl()
+        ),
+        new Cafe(
+          faker.datatype.number(),
+          faker.lorem.words(2),
+          faker.lorem.slug(),
+          faker.address.city(),
+          faker.lorem.sentence(),
+          faker.datatype.number(),
+          faker.image.imageUrl()
+        ),
+        new Cafe(
+          faker.datatype.number(),
+          faker.lorem.words(2),
+          faker.lorem.slug(),
+          faker.address.city(),
+          faker.lorem.sentence(),
+          faker.datatype.number(),
+          faker.image.imageUrl()
+        ),
+      ];
+    }
+
     fixture.detectChanges();
     debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a table element ', () => {
+    let trs = fixture.nativeElement.querySelectorAll('table');
+    expect(trs).toBeTruthy();
+    expect(trs.length).toBe(1);
+  });
+
+  it('should have a thead element ', () => {
+    let trs = fixture.nativeElement.querySelectorAll('thead');
+    expect(trs).toBeTruthy();
+    expect(trs.length).toBe(1);
+  });
+
+  it('should have a tbody element ', () => {
+    let trs = fixture.nativeElement.querySelectorAll('tbody');
+    expect(trs).toBeTruthy();
+    expect(trs.length).toBe(1);
+  });
+
+  it('should have a tr element ', () => {
+    let trs = fixture.nativeElement.querySelectorAll('tr');
+    expect(trs).toBeTruthy();
+    expect(trs.length).toBe(4);
   });
 });
